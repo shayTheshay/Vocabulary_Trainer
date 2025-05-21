92f825da76dfafc1267af18ef61e365a976bbc9a
shay solomon <shays433@gmail.com>
2025-05-21 19:56:19 +0300

    changes to training mode, implemented, and changes made.

--- constants.py
+++ constants.py
@@ -1,1 +1,15 @@
-json_name = 'vocabulary.json'
+import json
+
+json_name = 'vocab.json'
+
+
+def load_vocab():
+    with open(json_name, 'r', encoding= 'utf-8') as f:
+        return json.load(f)
+    
+def save_vocab(data):
+    with open(json_name, 'w', encoding= 'utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False) 
+
+
+times_for_word_review = 7
--- editing.py
+++ editing.py
@@ -1,14 +1,15 @@
 import json
 import os
-from constants import json_name
+from constants import json_name, load_vocab, save_vocab
 
 
 def editing_mode():
     print(f"\nEditing Mode")
     if json_exist() == False :
         create_json_file()
-    term = input(f"1 for adding a word\n2 for deleting a word\n3 update a word\n4 to list words\nEnter: ")
-    unit:str = input("Enter the unit: ").lower()
+    term = input(f"1 for adding a word\n2 for deleting a word\n3 update a word\n4 to list words\nAnything else to exit\nEnter: ")
+    if term in ("1" or "2" or "3" or "4"):
+        unit:str = input("Enter the unit: ").lower()
 
     match term:
         case "1":
@@ -52,8 +53,7 @@ def unit_exist_in_data(data:json, unit:str):
     return exist
 
 def add_word(unit:str, word:str, meaning:str):
-    with open(json_name, 'r') as f:
-        data = json.load(f)
+    data = load_vocab()
     if unit_exist_in_data(data, unit) == False:
         create_unit_answer = input("Would you like to create it for yes enter 1, if not anything else? ")
         if create_unit_answer != "1":
@@ -74,14 +74,12 @@ def add_word(unit:str, word:str, meaning:str):
     }
     data[unit].append(new_word)
 
-    with open(json_name, 'w') as f:
-        json.dump(data, f, indent=4)
+    save_vocab()
     return None
     
 
 def delete_word(unit:str, word:str):
-    with open(json_name, 'r') as f:
-        data = json.load(f)
+    data = load_vocab()
 
     if unit not in data:
         print(f"There is not a unit with the name {unit}")
@@ -91,13 +89,11 @@ def delete_word(unit:str, word:str):
                 data[unit].remove(entry)
                 break
     
-    with open(json_name, 'w') as f:
-        json.dump(data, f, indent=4)
+    save_vocab()
     return None
 
 def update_word(unit:str, old_word:str, new_word:str, new_meaning:str):
-    with open(json_name, 'r') as f:
-        data = json.load(f)
+    data = load_vocab()
 
     if unit_exist_in_data(data, unit) and word_exist_in_unit(data, unit, old_word):
         for entry in data[unit]:
@@ -105,13 +101,11 @@ def update_word(unit:str, old_word:str, new_word:str, new_meaning:str):
                 entry["word"] = new_word
                 entry["meaning"] = new_meaning
                 break
-        with open(json_name, 'w') as f:
-            json.dump(data, f, indent=4) 
+        save_vocab() 
     return None
 
 def list_words(unit:str):
-    with open(json_name, 'r') as f:
-        data = json.load(f)
+    data = load_vocab()
 
     if unit_exist_in_data(data, unit):
         for entry in data[unit]:
